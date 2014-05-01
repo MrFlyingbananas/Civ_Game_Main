daf5e23508c5f3e88b656cfb7eb060a4bdfdd2a3
UnkownUSer1001 <bryant.josh@outlook.com>
2014-05-01 15:27:05 -0500

    Added tile var values

--- src/civGame/Tile_Vars.java
+++ src/civGame/Tile_Vars.java
@@ -1,15 +1,14 @@
 package civGame;
 
 public class Tile_Vars {
-	public final static int HOUSE_GOLD_COST = 4;
-	public final static int HOUSE_PEOPLE_YIELD = 4;
-	public final static int MINE_PEOPLE_COST = 4;
-	public final static int MINE_GOLD_YIELD = 4;
-	public final static int MINE_STONE_YIELD = 4;
-	public final static int FARM_PEOPLE_COST = 4;
-	public final static int FARM_FOOD_YILED = 4;
-	public final static int FARM_GOLD_COST = 4;
-	public final static int WELL_STONE_COST = 4;
-	public final static int WELL_WATER_YILED = 4;
-	q2qeweqqwe
+	public final static int HOUSE_GOLD_COST = 60;
+	public final static int HOUSE_PEOPLE_YIELD = 10;
+	public final static int MINE_PEOPLE_COST = 5;
+	public final static int MINE_GOLD_YIELD = 80;
+	public final static int MINE_STONE_YIELD = 25;
+	public final static int FARM_PEOPLE_COST = 2;
+	public final static int FARM_FOOD_YILED = 16;
+	public final static int FARM_GOLD_COST = 25;
+	public final static int WELL_STONE_COST = 30;
+	public final static int WELL_WATER_YILED = 16;
 }
